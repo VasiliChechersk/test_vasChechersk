1ae0de2ca783b61c8e1e00252cd79b115e7923ed
VasiliChechersk <80296448112@mail.ru>
2023-03-14 14:55:57 +0300

    form functionality

--- Script/script.js
+++ Script/script.js
@@ -16,16 +16,46 @@ const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', '
 const select3 = document.querySelector('#month')
 let innerMonth = ''
 for (let el of month) {
-innerMonth +=`<option>${el}</option>`
+    innerMonth += `<option>${el}</option>`
 }
 select3.innerHTML = innerMonth
 
 
-const national =['American', 'Indian', 'Russian', 'Ukrainian', 'Mexican', 
-'Canadian', 'Iranian', 'Japanese', 'Chinese', 'Jew', 'Swedish', 'Norwegian', 'German', 'English', 'French']
+const national = ['American', 'Indian', 'Russian', 'Ukrainian', 'Mexican',
+    'Canadian', 'Iranian', 'Japanese', 'Chinese', 'Jew', 'Swedish', 'Norwegian', 'German', 'English', 'French']
 const selectNation = document.querySelector('#nationality')
 let innerNation = ''
 for (let el of national) {
-innerNation +=`<option>${el}</option>`
+    innerNation += `<option>${el}</option>`
 }
 selectNation.innerHTML = innerNation
+
+
+const form = document.forms.form
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const firstname = form.elements.firstname.value;
+    const lastname = form.elements.lastname.value;
+    const nationality = form.elements.nationality.value;
+    const email = form.elements.email.value;
+    const datebirth = form.elements.datebirth.value;
+    const monthbirth = form.elements.monthbirth.value;
+    const yearbirth = form.elements.yearbirth.value;
+    const radio = form.elements.radio.value;
+    const pass = form.elements.password.value;
+    const confirmpassword = form.elements.confirmpassword.value;
+
+   if (pass !== confirmpassword) {
+    document.querySelector('.pass_error').style.display='block'; 
+   }
+   else{
+    console.log([`FirstName: ${firstname}`,`LastName:${lastname}`,`Natonality:${nationality}`,
+    `Email:${email}`,`DateBirth:${datebirth}`,`MonthBirth:${monthbirth}`,`YearhBirth:${yearbirth}`,
+    `Gender:${radio}`,`Password:${pass}`]);
+   }
+   document.querySelector('.pass_error-btn').addEventListener('click', (e) =>{
+   document.querySelector('.pass_error').style.display='none';  
+})
+ 
+}
+)
